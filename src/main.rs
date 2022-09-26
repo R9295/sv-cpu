c06d3bc58be9221008fe0ba2c8ef8d004bf24692
aarnav <aarnavbos@gmail.com>
2022-09-26 16:51:44 +0200

    registers: switch from i32 to u32

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 struct CPU {
-    r0: i32,              // r0 = always 0
-    r1: i32,              // r1 = assembler temporary, reserved for assembler
-    r2: i32,              // r2, r3 = return value from a function call
-    r3: i32,              //
-    r4: i32,              // r4~r7 = first four parameters for a function call
-    r5: i32,              //
-    r6: i32,              //
-    r7: i32,              //
-    r8: i32,              // r8~r15 = temporary variables, need not be preserved
-    r9: i32,              //
-    r10: i32,             //
-    r11: i32,             //
-    r12: i32,             //
-    r13: i32,             //
-    r14: i32,             //
-    r15: i32,             //
-    r16: i32,             // r16~r23 = function variables, need to be preserved
-    r17: i32,             //
-    r18: i32,             //
-    r19: i32,             //
-    r20: i32,             //
-    r21: i32,             //
-    r22: i32,             //
-    r23: i32,             //
-    r24: i32,             // r24~r25 = temporary variables
-    r25: i32,             //
-    r26: i32,             // r26, r27 = reserved for OS
-    r27: i32,             //
-    r28: i32,             // r28 = global pointer
-    r29: i32,             // r29 = stack pointer
-    r30: i32,             // r30 = stack frame pointer or subroutine variable
-    r31: i32,             // r31 = return address of last subroutine call
+    r0: u32,              // r0 = always 0
+    r1: u32,              // r1 = assembler temporary, reserved for assembler
+    r2: u32,              // r2, r3 = return value from a function call
+    r3: u32,              //
+    r4: u32,              // r4~r7 = first four parameters for a function call
+    r5: u32,              //
+    r6: u32,              //
+    r7: u32,              //
+    r8: u32,              // r8~r15 = temporary variables, need not be preserved
+    r9: u32,              //
+    r10: u32,             //
+    r11: u32,             //
+    r12: u32,             //
+    r13: u32,             //
+    r14: u32,             //
+    r15: u32,             //
+    r16: u32,             // r16~r23 = function variables, need to be preserved
+    r17: u32,             //
+    r18: u32,             //
+    r19: u32,             //
+    r20: u32,             //
+    r21: u32,             //
+    r22: u32,             //
+    r23: u32,             //
+    r24: u32,             // r24~r25 = temporary variables
+    r25: u32,             //
+    r26: u32,             // r26, r27 = reserved for OS
+    r27: u32,             //
+    r28: u32,             // r28 = global pointer
+    r29: u32,             // r29 = stack pointer
+    r30: u32,             // r30 = stack frame pointer or subroutine variable
+    r31: u32,             // r31 = return address of last subroutine call
     memory: [u8; 65_535], // 2 ^ 16 - 1
 }
 impl Default for CPU {
